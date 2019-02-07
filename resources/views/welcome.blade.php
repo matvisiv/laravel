a220bf3f82d05031f531ef29e83f18aa37c325f6
matvisiv <matvijko94@ukr.net>
2019-02-07 17:50:14 +0200

    qwerty

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LALLALALRAVEL
+                    LRAVEL
                 </div>
 
                 <div class="links">
