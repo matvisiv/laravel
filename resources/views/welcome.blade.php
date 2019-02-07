8565556cd62f997886d7cbd3e4c000f8694e8882
matvisiv <matvijko94@ukr.net>
2019-02-07 15:15:46 +0200

    alal commit 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LALALALARAveL
+                    ALALALARAveL
                 </div>
 
                 <div class="links">
