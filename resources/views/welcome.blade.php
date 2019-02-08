b0929b08250956f704ea8482e07bafcf6951a725
matvijko <matvijko94@ukr.net>
2019-02-08 11:12:28 +0200

    added new file index.js 08.02.201911:12:28,24

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LRAVEL
+                    allalLALLRAVEL
                 </div>
 
                 <div class="links">
