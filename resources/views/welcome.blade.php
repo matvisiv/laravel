67ea7e062f95908a76d15b8fba1b95780947d6d2
matvisiv <matvijko94@ukr.net>
2019-02-08 11:51:23 +0200

    'COMMIT--08.02.2019--11:51:23,51--'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,12 +73,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    aLALLRAVEL
+                    LALALARAVEL
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Lardddddcasts</a>
+                    <a href="https://laracasts.com">Larcasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
