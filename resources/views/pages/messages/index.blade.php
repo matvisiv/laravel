eced4f1a9c5dcc5a193981bf631ef53ffcb51a35
matvisiv <matvijko94@ukr.net>
2019-02-11 15:43:49 +0200

    'COMMIT--11.02.2019--15:43:49,45--'

--- resources/views/pages/messages/index.blade.php
+++ resources/views/pages/messages/index.blade.php
@@ -1,9 +1,28 @@
      @extends('index')
 
     @section('content')
-    @include('_common._form')
+    <form method="POST" id="id-form_messages">
 
-        <div class="text-right"><b>Всего сообщений:</b> <i class="badge">{{$count}}</i></div>
+        <div class="form-group">
+            <label for="name">Имя: *</label>
+            <input class="form-control" placeholder="Имя" name="name" type="text" id="name">
+        </div>
+
+        <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input class="form-control" placeholder="E-mail" name="email" type="email" id="email">
+        </div>
+
+        <div class="form-group">
+            <label for="message"></label>
+            <textarea class="form-control" rows="5" placeholder="Текст сообщения" name="message" cols="50"  id="message"></textarea>
+        </div>
+
+        <div class="form-group">
+            <input class="btn btn-primary" type="submit" value="Добавить">
+        </div>
+
+        <div class="text-right"><b>Всего сообщений:</b> <i class="badge">0</i></div>
         <br/>
 
         <div class="messages">
