b2d948dc9f66813066e4ab77163d98f528405f00
matvisiv <matvijko94@ukr.net>
2019-02-13 17:55:03 +0200

    'COMMIT--13.02.2019--17:55:03,39--'

--- resources/views/_common/_form.blade.php
+++ resources/views/_common/_form.blade.php
@@ -1,4 +1,4 @@
-    <form method="POST" action="{{URL::to('/form')}}" id="id-form_messages">
+    <form method="POST" action="/form" id="id-form_messages">
         <div class="form-group">
             <label for="name">Имя: *</label>
             <input class="form-control" placeholder="Имя" name="name" type="text" id="name">
