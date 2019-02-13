a76097f12fd0b9bad33b84f205e10093f0e98947
matvisiv <matvijko94@ukr.net>
2019-02-13 15:56:34 +0200

    'COMMIT--13.02.2019--15:56:33,84--'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
 use App\Models\Message;
 
 class HomeController extends Controller
