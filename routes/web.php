2e9a3934d70621d3b7e0776f8cfb6623a4e94d08
matvisiv <matvijko94@ukr.net>
2019-02-13 17:48:20 +0200

    'COMMIT--13.02.2019--17:48:20,23--'

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', ['uses' => 'HomeController@index','as' => 'home']);
 #Route::get('message/{id}/edit', ['uses' => 'HomeController@edit','as' => 'message.edit']);
 
 
-Route::any('/form', 'HomeController@add');
+Route::post('/form', 'HomeController@add');
 
 
 
