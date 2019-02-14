14601dad267f4c127840c738b287cb2d15b892cc
matvisiv <matvijko94@ukr.net>
2019-02-14 16:43:25 +0200

    Start commit

--- storage/framework/views/150e3f1374840c0ec4025426d1c62ec0ebf041c0.php
+++ storage/framework/views/150e3f1374840c0ec4025426d1c62ec0ebf041c0.php
@@ -2,8 +2,7 @@
 
     <?php $__env->startSection('content'); ?>
     <?php echo $__env->make('_common._form', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-    <div class="text-right"><b>Всего сообщений:</b> <i class="badge"><?php echo e($count); ?></i></div>
-    <br/>
+
     <?php echo $__env->make('pages.messages._items', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
 
     <?php $__env->stopSection(); ?>
