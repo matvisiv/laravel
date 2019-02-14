ccd88396b259e5d08fcd36b50453d9e73b9999b6
matvisiv <matvijko94@ukr.net>
2019-02-14 17:26:58 +0200

    'COMMIT--14.02.2019--17:26:57,90--'

--- resources/views/pages/messages/edit.blade.php
+++ resources/views/pages/messages/edit.blade.php
@@ -1,5 +1,30 @@
     @extends('index')
 
     @section('content')
-    @include('_common._form')
+    	@if ( ! $id->isEmpty() )
+
+	    <form method="POST" action="{{URL::to('/edit')}}" id="id-form_edit">
+	        <div class="form-group">
+	            <label for="name">Имя: *</label>
+	            <input class="form-control" placeholder="Имя" value="{!! $message->name !!}" name="name" type="text" id="name">
+	        </div>
+
+	        <div class="form-group">
+	            <label for="email">E-mail:</label>
+	            <input class="form-control" value="{!! $message->email !!}" name="email" type="email" id="email">
+	        </div>
+
+	        <div class="form-group">
+	            <label for="messages">Сообщение: *</label>
+	            <textarea class="form-control" rows="5" value="{!! $message->messages !!}" name="messages" cols="50"  id="messages"></textarea>
+	        </div>
+	        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+	        <div class="form-group">
+	            <input class="btn btn-primary" type="submit" value="Добавить">
+	        </div>
+	        <hr>
+
+   		 </form>
+    @endif
+
     @stop
