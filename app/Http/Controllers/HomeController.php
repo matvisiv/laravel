7a874857dc6111784a075d4c383bee2ca55ddc65
matvisiv <matvijko94@ukr.net>
2019-02-14 10:21:33 +0200

    'COMMIT--14.02.2019--10:21:33,47--'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,9 +31,9 @@ class HomeController extends Controller
     public function add(Request $request)
     {
         $ddd = new Message;
-        $ddd->name = $request->Input('name');
-        $ddd->email = $request->Input('email');
-        $ddd->messages = $request->Input('messages');
+        $ddd->name = $request->Input::post('name');
+        $ddd->email = $request->Input::post('email');
+        $ddd->messages = $request->Input::post('messages');
         $ddd->save();
         Return view('pages.messages.index');
     }
