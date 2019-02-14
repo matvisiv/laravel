62f7a9b5f62efdf6820542d0183467b4a97ea5c3
matvisiv <matvijko94@ukr.net>
2019-02-14 10:29:30 +0200

    'COMMIT--14.02.2019--10:29:30,11--'

--- resources/views/pages/messages/index.blade.php
+++ resources/views/pages/messages/index.blade.php
@@ -2,8 +2,7 @@
 
     @section('content')
     @include('_common._form')
-    <div class="text-right"><b>Всего сообщений:</b> <i class="badge">{{ $count }}</i></div>
-    <br/>
+
     @include('pages.messages._items')
 
     @stop
