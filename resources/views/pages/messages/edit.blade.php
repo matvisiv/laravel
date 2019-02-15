573450c4856024d54c8e8a5fbe3daba60c115ae8
matvisiv <matvijko94@ukr.net>
2019-02-15 12:56:05 +0200

    'COMMIT--15.02.2019--12:56:05,52--'

--- resources/views/pages/messages/edit.blade.php
+++ resources/views/pages/messages/edit.blade.php
@@ -4,7 +4,7 @@
 	    <form method="POST" action="{{URL::to('/edit')}}" id="id-form_edit">
 	        <div class="form-group">
 	            <label for="name">Имя: *</label>
-	            <input class="form-control" placeholder="Имя" value="{{ $data->name }}" name="name" type="text" id="name">
+	            <input class="form-control" placeholder="Имя" value="{!! $data->name !!}" name="name" type="text" id="name">
 	        </div>
 
 	        <div class="form-group">
