94dc7a3245bc620226179f3499a41b88b4c841d3
matvisiv <matvijko94@ukr.net>
2019-02-16 22:27:44 +0200

    'COMMIT--16.02.2019--22:27:43,98--'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,6 +41,6 @@ class HomeController extends Controller
     public function edit($id)
     {
         $old = Message::find($id);
-        return view('pages.messages.edit'); 
+        return view('pages.messages.edit', $old); 
     }
 }
