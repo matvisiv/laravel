3efc88b17ebfa821adb7af5576ea1619632f893b
matvisiv <matvijko94@ukr.net>
2019-02-16 22:37:26 +0200

    'COMMIT--16.02.2019--22:37:26,34--'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,6 +41,13 @@ class HomeController extends Controller
     public function edit($id)
     {
         $old = Message::find($id);
-        return view('pages.messages.edit', $old); 
+        $data = [
+        'title' => 'Guestbook',
+        'pagetitle' => 'LALALAraveL',
+        'messages' => Message::latest()->paginate(4),
+        'count' => message::count()
+    ];
+        return view('pages.messages.edit', $old, $data);
+
     }
 }
--- resources/views/pages/messages/edit.blade.php
+++ resources/views/pages/messages/edit.blade.php
@@ -1,6 +1,25 @@
     @extends('index')
     @section('content')
-<?php
-	echo $id;
-?>
+	    <form method="POST" route action="{{URL::to('/edit')}}" id="id-form_edit">
+	        <div class="form-group">
+	            <label for="name">Имя: *</label>
+	            <input class="form-control" value="{{ $old->name }}" type="text" id="name">
+	        </div>
+
+	        <div class="form-group">
+	            <label for="email">E-mail:</label>
+	            <input class="form-control" value="{{ $old->email }}" name="email" type="email" id="email">
+	        </div>
+
+	        <div class="form-group">
+	            <label for="messages">Сообщение: *</label>
+	            <textarea class="form-control" rows="5" value="{{ $old->messages }}" name="messages" cols="50"  id="messages"></textarea>
+	        </div>
+	        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+	        <div class="form-group">
+	            <input class="btn btn-primary" type="submit" value="Добавить">
+	        </div>
+	        <hr>
+
+   		 </form>
     @stop
