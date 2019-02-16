475d431db9f4bdb2581d4cc084d05729b5c854d6
matvisiv <matvijko94@ukr.net>
2019-02-16 22:24:25 +0200

    'COMMIT--16.02.2019--22:24:25,24--'

--- resources/views/pages/messages/edit.blade.php
+++ resources/views/pages/messages/edit.blade.php
@@ -1,8 +1,5 @@
     @extends('index')
     @section('content')
-    @if ( ! $old->isEmpty() )
-    	foreach ($old)
-		{
 	    <form method="POST" route action="{{URL::to('/edit')}}" id="id-form_edit">
 	        <div class="form-group">
 	            <label for="name">Имя: *</label>
@@ -25,6 +22,4 @@
 	        <hr>
 
    		 </form>
-   		}
-	@endif
     @stop
