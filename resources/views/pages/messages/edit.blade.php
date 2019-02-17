154b2e0b020fd5e3a62f096bc65bd4b1e3074537
matvisiv <matvijko94@ukr.net>
2019-02-17 12:45:00 +0200

    'COMMIT--17.02.2019--12:45:00,27--'

--- resources/views/pages/messages/edit.blade.php
+++ resources/views/pages/messages/edit.blade.php
@@ -8,12 +8,12 @@
 
 	        <div class="form-group">
 	            <label for="email">E-mail:</label>
-	            <input class="form-control" value="{{ $oldd->email }}" name="email" type="email" id="email">
+	            <input class="form-control" value="{!! $oldd->email !!}" name="email" type="email" id="email">
 	        </div>
 
 	        <div class="form-group">
 	            <label for="messages">Сообщение: *</label>
-	            <textarea class="form-control" rows="5" value="{{ $oldd->messages }}" name="messages" cols="50"  id="messages"></textarea>
+	            <textarea class="form-control" rows="5" value="{!! $oldd->messages !!}" name="messages" cols="50"  id="messages"></textarea>
 	        </div>
 	        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 	        <div class="form-group">
